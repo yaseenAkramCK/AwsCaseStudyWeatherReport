d75f21dd972121d8deef7cfe3204aff18cd0a970
Yaseen Akram C K <yaseen.61079421@ltimindtree.com>
2024-10-23 15:27:08 +0530

    deployment12

--- api.js
+++ api.js
@@ -8,14 +8,16 @@ const {
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
-const getWeatherReport = async (event) => {
+const getWeatherReport = async (event) => { 
     const response = { statusCode: 200 };
 
     try {
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({ name: event.pathParameters.name }),
-              //key:({name: event.pathParameters.name })  
+            Key: marshall(
+                { id: event.pathParameters.id },
+                { name: event.pathParameters.name }
+            ),
         };
         const { Item } = await db.send(new GetItemCommand(params));
 
