fd3ae1bf31f2dd7c81b06daea1ecc74918cb03b7
Yaseen Akram C K <yaseen.61079421@ltimindtree.com>
2024-10-23 14:27:53 +0530

    Deployment6

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ const getWeatherReport = async (event) => {
     try {
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({ id: event.pathParameters.id }),
+            Key: marshall({ name: event.pathParameters.name }),
         };
         const { Item } = await db.send(new GetItemCommand(params));
 
@@ -73,7 +73,7 @@ const updateWeatherReport = async (event) => {
         const objKeys = Object.keys(body);
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({ id: event.pathParameters.id }),
+            Key: marshall({ name: event.pathParameters.name }),
             UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
             ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
                 ...acc,
@@ -109,7 +109,7 @@ const deleteWeatherReport = async (event) => {
     try {
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({ id: event.pathParameters.id }),
+            Key: marshall({ name: event.pathParameters.name }),
         };
         const deleteResult = await db.send(new DeleteItemCommand(params));
 
